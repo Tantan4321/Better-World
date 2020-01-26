110a9541e7bcbbcaa53cd03a8aa9be5fe65a9e03
jrajan15 <johnny.rajan@gmail.com>
2020-01-26 05:02:59 -0600

    white text

--- minnehack_app/lib/configs/AppColors.dart
+++ minnehack_app/lib/configs/AppColors.dart
@@ -6,4 +6,5 @@ class AppColors {
   static const Color oceanBlue = Color(0xFF53a2be);
   static const Color blue = Color(0xFF1d84b5);
   static const Color darkBlue = Color(0xFF132e32);
+  static const Color white = Color(0xFFFFFFFF);
 }
