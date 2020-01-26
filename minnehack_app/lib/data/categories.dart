f77cfc1d8dcddc0c67e5aacf27663b62a5aff2bc
jrajan15 <johnny.rajan@gmail.com>
2020-01-26 04:48:21 -0600

    changed colors 2

--- minnehack_app/lib/data/categories.dart
+++ minnehack_app/lib/data/categories.dart
@@ -3,14 +3,14 @@ import 'package:minnehack_app/models/category.dart';
 import 'package:minnehack_app/configs/AppColors.dart';
 
 const List<Category> categories = [
-  Category(name: "Fundraising", color: AppColors.darkTeal),
-  Category(name: "Environmental", color: AppColors.navyBlue),
-  Category(name: "Children/Youth", color: AppColors.oceanBlue),
-  Category(name: "Elderly", color: AppColors.blue),
-  Category(name: "Cleanup", color: AppColors.darkBlue),
-  Category(name: "Education", color: AppColors.darkTeal),
-  Category(name: "Coaching/Teaching", color: AppColors.navyBlue),
-  Category(name: "Food Preparation", color: AppColors.oceanBlue),
-  Category(name: "Social Justice", color: AppColors.blue),
-  Category(name: "Emergency Relief", color: AppColors.darkBlue),
+  Category(name: "Fundraising", color: AppColors.navyBlue),
+  Category(name: "Environmental", color: AppColors.oceanBlue),
+  Category(name: "Children/Youth", color: AppColors.blue),
+  Category(name: "Elderly", color: AppColors.darkBlue),
+  Category(name: "Cleanup", color: AppColors.navyBlue),
+  Category(name: "Education", color: AppColors.oceanBlue),
+  Category(name: "Coaching/Teaching", color: AppColors.blue),
+  Category(name: "Food Preparation", color: AppColors.darkBlue),
+  Category(name: "Social Justice", color: AppColors.navyBlue),
+  Category(name: "Emergency Relief", color: AppColors.oceanBlue),
 ];
