f43f20b1eaaa87a4883a79c9f28d92958d984fc2
jrajan15 <johnny.rajan@gmail.com>
2020-01-26 03:22:48 -0600

    addedFormatForData

---
+++ minnehack_app/lib/data/dataForCategories.dart
@@ -0,0 +1,36 @@
+import 'package:minnehack_app/models/ResultsForEachCategory.dart';
+import 'package:minnehack_app/models/category.dart';
+import 'package:minnehack_app/models/organization.dart';
+import 'package:minnehack_app/ui/AppColors.dart';
+
+const List<results> Res = [
+    results(
+        title: "Be an advocate. Fight Poverty",
+        description:
+    ),
+  ];
+String c =
+    "Do you wish that you could have a say in the policies that impact millions of people living in poverty here in the United States and around the world? Are you feeling overwhelmed by this political moment? Maybe you’ve been feeling the urge to do something but aren’t sure what..."
+    "As a RESULTS advocate, you can stand up for issues of poverty, be heard, and make an impact. At RESULTS, we teach you the nuts and bolts of how to do that. We support you to be an advocate.
+
+RESULTS is a movement of passionate, committed, everyday people. Together we use our voices to influence political decisions that will bring an end to poverty.
+
+As a RESULTS volunteer, you will be an effective advocate in the fight against poverty.
+
+Practically, that means:
+
+Developing a relationship with your members of Congress so that when you get in touch, they listen.
+Meeting face to face with your members of Congress and their staff.
+Writing persuasive opinion pieces in the media.
+Getting support from your local RESULTS group monthly for training, updates, and action.
+Being part of a movement of people committed to ending the systemic and structural injustices that drive poverty.
+No advocacy experience? No problem. RESULTS provides comprehensive issue and advocacy skills training to all volunteers.
+
+We’ve been doing this for 40 years, and we know that change doesn’t happen just because we vote. It happens because you tell your members of Congress what you care about and what they can do about it. It happens because you make calls, you write letters, and you meet with decision makers face to face. Change happens because you demand it to happen.
+
+What are you waiting for?
+
+Step up. Speak out. Become a RESULTS advocate.
+
+Learn more at results.org.
+";
